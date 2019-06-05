60576af7785b86945cf96835eb117684d2c8b05a
zxt <825603082@qq.com>
2019-06-05 20:05:20 +0800

    李四修改了代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("Hellojava!");
-        System.out.println("lisi!");
+        System.out.println("lisi修改的代码!");
 		
     }
 }
